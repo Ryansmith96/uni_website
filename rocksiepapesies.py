6de562409f7042f90dd166d09a0da24f9c8a49fd
Ryansmith96 <ryan.j.smith.2019@gmail.com>
2020-02-26 11:19:43 +0000

    Added computer learning to rockpapes

--- rocksiepapesies.py
+++ rocksiepapesies.py
@@ -7,65 +7,92 @@ computerguess = ""
 userscore = 0
 computerscore = 0
 computerlist = ["rock", "paper", "scissors"]
-computerguess = random.choice(computerlist)
 game = True 
 
+# machine learning append
+rock_counter = 0
+paper_counter = 0
+scissors_counter = 0
+
+
+def better_decision ():
+    max_counter = max(rock_counter, paper_counter, scissors_counter)
+    if rock_counter == max_counter:
+        return "paper"
+    elif paper_counter == max_counter:
+        return "scissors"
+    elif scissors_counter == max_counter:
+        return "rock"
+    else:
+        random.choice(computerlist)
+
+#computerguess = better_decision()
   
 
 # the game
 while game == True:
     userguess = str(input("rock, paper, scissors (press q to quit):  "))
+    computerguess = better_decision()
     if userguess == "rock" and computerguess == "rock":
         print("Computer also played rock: Tie") 
         print("Your score is: " + str(userscore)) 
         print("Computer score is: " + str(computerscore))
-        computerguess = random.choice(computerlist)
+        #computerguess = random.choice(computerlist)
+        rock_counter +=1
     elif userguess == "rock" and computerguess == "paper":
         print("Computer played paper: You Lose")
         computerscore = computerscore + 1
         print("Your score is: " + str(userscore)) 
         print("Computer score is: " + str(computerscore))
-        computerguess = random.choice(computerlist)
+        #computerguess = random.choice(computerlist)
+        rock_counter +=1
     elif userguess == "rock" and computerguess == "scissors":
         print("Computer played scissors: You Win!")
         userscore = userscore + 1
         print("Your score is: " + str(userscore)) 
         print("Computer score is: " + str(computerscore))
-        computerguess = random.choice(computerlist)
+        #computerguess = random.choice(computerlist)
+        rock_counter +=1
     elif userguess == "paper" and computerguess == "rock":
         print("Computer played rock: You Win!")
         userscore = userscore + 1
         print("Your score is: " + str(userscore)) 
         print("Computer score is: " + str(computerscore))
-        computerguess = random.choice(computerlist)
+        #computerguess = random.choice(computerlist)
+        paper_counter +=1
     elif userguess == "paper" and computerguess == "paper":
         print("Computer also played paper: Tie")
         print("Your score is: " + str(userscore)) 
         print("Computer score is: " + str(computerscore))
-        computerguess = random.choice(computerlist)
+        #computerguess = random.choice(computerlist)
+        paper_counter +=1
     elif userguess == "paper" and computerguess == "scissors":
         print("Computer played scissors: You Lose")
         computerscore = computerscore + 1
         print("Your score is: " + str(userscore)) 
         print("Computer score is: " + str(computerscore))
-        computerguess = random.choice(computerlist)
+        #computerguess = random.choice(computerlist)
+        paper_counter +=1
     elif userguess == "scissors" and computerguess == "rock":
         print("Computer played rock: You Lose")
         computerscore = computerscore + 1
         print("Your score is: " + str(userscore)) 
         print("Computer score is: " + str(computerscore))
-        computerguess = random.choice(computerlist)
+        #computerguess = random.choice(computerlist)
+        scissors_counter +=1
     elif userguess == "scissors" and computerguess == "paper":
         print("Computer played paper: You Win!")
         userscore = userscore + 1
         print("Your score is: " + str(userscore)) 
         print("Computer score is: " + str(computerscore))
-        computerguess = random.choice(computerlist)
+        #computerguess = random.choice(computerlist)
+        scissors_counter +=1
     elif userguess == "scissors" and computerguess == "scissors":
         print("Computer played scissors: Tie")
         print("Your score is: " + str(userscore)) 
         print("Computer score is: " + str(computerscore))
-        computerguess = random.choice(computerlist)
+       # computerguess = random.choice(computerlist)
+        scissors_counter +=1
     elif userguess == "q":
         game = False
     elif userguess != "rock" or "paper" or "scissors" or "q":
@@ -76,3 +103,5 @@ while game == True:
     elif computerscore == 3:
         game = False
         print("You lost to a machine :(")
+
+
